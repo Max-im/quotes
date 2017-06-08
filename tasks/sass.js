a4caa85b90a4e3bf98393ddf4880538077c9a6e4
Pozhidaev Maxim <pogidaevmo@gmail.com>
2017-06-08 23:25:48 +0300

    add gulp-import-css in sass task

--- tasks/sass.js
+++ tasks/sass.js
@@ -6,12 +6,12 @@ const sass 			= require('gulp-sass');
 const browserSync 	= require('browser-sync');
 const cssnano 		= require('gulp-cssnano');
 const rename 		= require('gulp-rename');
-
 const debug 		= require('gulp-debug');
 const sourcemaps	= require('gulp-sourcemaps');
 const multipipe		= require('multipipe');
 const notify		= require('gulp-notify');
 const autoprefixer 	= require('gulp-autoprefixer'); 
+const importCss		= require('gulp-import-css'); 
 
 
 module.exports = function(options){
@@ -31,6 +31,7 @@ module.exports = function(options){
 	  			'iOS >= 6',
 	  			'Opera >= 12',
 	  			'Safari >= 6'], { cascade: true }),
+			importCss(),
 			cssnano(),
 			rename('style.min.css'),
 			sourcemaps.write(),
